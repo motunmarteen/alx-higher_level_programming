ca09024a21d31a2fd59f8abeab097594c2165ede
Motun Marteen <111828215+marteenmotun@users.noreply.github.com>
2023-01-02 18:46:23 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""
-Function that prints My name is <first name> <last name>
-"""
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints a Name
-    Arguments:
-    first_name: must be a string
-    last_name: must be a string, if no argument given, then empty by default
-    Otherwise a TypeError is raised
+    """Print a name.
+
+    Args:
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
     """
     if not isinstance(first_name, str):
-        msg = "first_name must be a string"
-        raise TypeError(msg)
-
+        raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        msg = "last_name must be a string"
-        raise TypeError(msg)
-
-    print("My name is {} {}".format(first_name, last_name))                                                                                        if len(set(checkl)) != 1:
-                                                                                                                            msg = "Each row of the matrix must have the same size"
-                                                                                                                                    raise TypeError(msg)
-
-                                                                                                                                    if not isinstance(div, (int, float)) or div != div:
-                                                                                                                                                msg = "div must be a number"
-                                                                                                                                                        raise TypeError(msg)
-
-                                                                                                                                                        if div == 0:
-                                                                                                                                                                    msg = "division by zero"
-                                                                                                                                                                            raise ZeroDivisionError(msg)
-
-                                                                                                                                                                            new_m = [[round(j / div, 2) for j in i] for i in matrix]
-                                                                                                                                                                                return (new_m)
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
