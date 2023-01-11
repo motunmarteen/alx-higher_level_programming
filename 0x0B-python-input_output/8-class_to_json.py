54e25875dfa44ef3e57087341393b3ba7e183749
Motun Marteen <111828215+marteenmotun@users.noreply.github.com>
2023-01-11 23:18:04 +0100

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+"""This module defines a Python class-to-JSON function"""
 
 
 def class_to_json(obj):
-    """ function that returns the dictionary description with simple data
-    structure (list, dictionary, string, integer and boolean) for JSON
-    serialization of an object:
-    """
-    dic = {}
-    if hasattr(obj, "__dict__"):
-        dic = obj.__dict__.copy()
-    return dic
+    """Returns the dictionary representation of a simple data structure"""
+    return obj.
