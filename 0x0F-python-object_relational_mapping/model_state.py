533b3be2ce15d0252d2df13aa90a16a1d5808354
Motun Marteen <111828215+marteenmotun@users.noreply.github.com>
2023-03-22 19:18:39 +0100

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+"""Script to create a class State
 """
-This script defines a State class and
-a Base class to work with MySQLAlchemy ORM.
-"""
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,13 +8,10 @@ Base = declarative_base()
 
 
 class State(Base):
-        """State class
-            Attributes:
-                    __tablename__ (str): The table name of the class
-                            id (int): The State id of the class
-                                    name (str): The State name of the class
-                                        """
-                                            __tablename__ = 'states'
-
-                                                id = Column(Integer, primary_key=True)
-                                                    name = Column(String(128), nullable=False)
+    """Class definition of a state that inherits of Base class
+    Args:
+        Base : class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
